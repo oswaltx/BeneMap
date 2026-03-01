3dfc018e398ee435fe9c39ea8479d2e22b557ba1
David Oswalt <116766497+oswaltx@users.noreply.github.com>
2026-03-02 00:08:01 +0100

    Datenmodell erweitert

--- backend/src/main/kotlin/com/example/VoloMap/server/Marker.kt
+++ backend/src/main/kotlin/com/example/VoloMap/server/Marker.kt
@@ -1,8 +1,14 @@
 package com.example.VoloMap.server
 
+import java.time.LocalDateTime
+
 data class Marker(
     val id: Long,
     val lat: Double,
     val lng: Double,
-    val name: String
+    val name: String,
+    val dateTime: LocalDateTime,
+    val address: String,
+    val category: String,
+    val description: String
 )
--- frontend/src/lib/Map.svelte
+++ frontend/src/lib/Map.svelte
@@ -1,12 +1,16 @@
 <script>
     import {onMount} from "svelte";
+    import { ControlZoom } from 'sveaflet';
     import {Map, Marker, Popup, TileLayer} from "sveaflet";
+    import Button from "./Button.svelte";
 
-    let markers = [{ id: 1, lat: 50.9375, lng: 6.9603, name: "Erroror 1" }];
+    let markers = [{ id: 1, lat: 50.9375, lng: 6.9603, name: "Erroror 1", address: "Erroror 1", dateTime: "2023-01-01T00:00:00Z" }];
 
-    async function fetchMarkers() {
+    async function fetchMarkers(dateFilter = "none", category = "none") {
+        console.log("dateFilter: "+dateFilter)
+        console.log("categoryFilter"+category)
         try {
-            const res = await fetch("http://localhost:8080/markers");
+            const res = await fetch("http://localhost:8080/markers?dateFilter=" + dateFilter + "&category="+ category);
             // Expecting something like:
             // [{ id: 1, lat: 50.9375, lng: 6.9603, name: "Marker 1" }]
             markers = await res.json();
@@ -34,6 +38,9 @@
                 <Popup>
                     <h3>{marker.name}</h3>
                     <p>Lat: {marker.lat}, Lng: {marker.lng}</p>
+                    <p>Address: {marker.address}</p>
+                    <p>Date: {marker.dateTime}</p>
+                    <p>ID: {marker.id}</p>
                 </Popup>
             </Marker>
         {/each}
