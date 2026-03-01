e93ec05cafd63aa65434379366975b7dfa1fc2c4
David Oswalt <116766497+oswaltx@users.noreply.github.com>
2026-03-02 00:06:38 +0100

    datums und kategoriefilter!

--- backend/src/main/kotlin/com/example/VoloMap/server/MainController.kt
+++ backend/src/main/kotlin/com/example/VoloMap/server/MainController.kt
@@ -1,8 +1,8 @@
 package com.example.VoloMap.server
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -43,15 +43,25 @@ val sampleMarkers = listOf(
 @CrossOrigin(origins = ["http://localhost:5173"])
 @RestController
 class MainController {
+
     @GetMapping("/")
     fun index() = "Hello World!"
+
     @GetMapping("/markers")
-    fun markers(): List<Marker> {
-        return listOf(
-            Marker(1, 50.9375, 6.9603, "Marker 1"),
-            Marker(2, 50.9335, 6.9503, "Marker 2"),
-            Marker(3, 50.9235, 6.9543, "Marker 3"),
-            Marker(4, 50.9175, 6.9603, "Marker 4"),
-        )
+    fun markers(@RequestParam dateFilter: String, @RequestParam category: String): List<Marker> {
+        var filterDate = LocalDate.of(2023, 10, 1)
+        try {
+            filterDate = LocalDate.parse(dateFilter) // Assuming dateFilter is in the format YYYY-MM-DD
+        }
+        catch (e: Exception) {
+            println(e)
+        }
+        //val today = LocalDate.now()
+
+        return if (category =="Umweltpflege") {
+            sampleMarkers.filter { it.dateTime.toLocalDate() == filterDate }
+        } else {
+            sampleMarkers
+        }
     }
 }
