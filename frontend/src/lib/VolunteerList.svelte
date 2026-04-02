36a48fbf6a8fff17a81d5e44d1a53845fe0ee491
david_windows <xd@oswalt.de>
2026-04-02 16:19:05 +0200

    Create VolunteerList.svelte to display markers with details.

---
+++ frontend/src/lib/VolunteerList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let markers: {
+        id: number;
+        name: string;
+        address: string;
+        category: string;
+        dateTime: string;
+        lat: number;
+        lng: number;
+    }[] = [];
+</script>
+
+
+    {#each markers as marker}
+            <strong>{marker.name}</strong>
+            <p>{marker.address}</p>
+            <p>{marker.category}</p>
+            <p>{new Date(marker.dateTime).toLocaleString("de-DE")}</p>
+    {/each}
