0cd0eae80e2dfb118ea6019dcb98e88e93a148f6
david_windows <xd@oswalt.de>
2026-04-09 16:44:24 +0200

    Update Map.svelte to use `CircleMarker`, enhance tile layer with attribution, and adjust map styling

--- frontend/src/lib/Map.svelte
+++ frontend/src/lib/Map.svelte
@@ -1,53 +1,69 @@
-<script lang="ts">
-    import {onMount} from "svelte";
-    import { ControlZoom } from 'sveaflet';
-    import {Map, Marker, Popup, TileLayer} from "sveaflet";
-    import Button from "./Button.svelte";
-    import FilterBar from "./FilterBar.svelte";
-    import VolunteerList from "./VolunteerList.svelte";
+    <script lang="ts">
+        import {onMount} from "svelte";
+        import { ControlZoom } from 'sveaflet';
+        import {Map, Marker, Popup, TileLayer} from "sveaflet";
+        import Button from "./Button.svelte";
+        import FilterBar from "./FilterBar.svelte";
+        import VolunteerList from "./VolunteerList.svelte";
+        import { CircleMarker } from "sveaflet";
 
 
-    let markers = [{category: "Brono", id: 1, lat: 50.9375, lng: 6.9603, name: "Erroror 1", address: "Erroror 1", dateTime: "2023-01-01T00:00:00Z" }];
-    let categories = ["Brono", "Kino", "Kultur", "Sport"];
-    onMount(async () => {
-        const res = await fetch("http://localhost:8080/categories");
-        categories = await res.json();
-        fetchMarkers();
-    });
 
-    async function fetchMarkers(date = "", category = "", timeFrom = "", timeTo = "") {
-        const params = new URLSearchParams();
-        if (date) params.append("date", date);
-        if (category) params.append("category", category);
-        if (timeFrom !== null) params.append("timeFrom", timeFrom);
-        if (timeTo !== null) params.append("timeTo", timeTo);
+        let markers = [{category: "Brono", id: 1, lat: 50.9375, lng: 6.9603, name: "Erroror 1", address: "Erroror 1", dateTime: "2023-01-01T00:00:00Z" }];
+        let categories = ["Brono", "Kino", "Kultur", "Sport"];
+        onMount(async () => {
+            const res = await fetch("http://localhost:8080/categories");
+            categories = await res.json();
+            fetchMarkers();
+        });
 
-        const res = await fetch("http://localhost:8080/markers?" + params.toString());
-        markers = await res.json();
-    }
+        async function fetchMarkers(date = "", category = "", timeFrom = "", timeTo = "") {
+            const params = new URLSearchParams();
+            if (date) params.append("date", date);
+            if (category) params.append("category", category);
+            if (timeFrom !== null) params.append("timeFrom", timeFrom);
+            if (timeTo !== null) params.append("timeTo", timeTo);
 
-    function handleFilter(event: CustomEvent<{ date: "", category: "", timeFrom: "", timeTo: "" }>) {
-        const { date, category, timeFrom, timeTo } = event.detail;
-        fetchMarkers(date, category, timeFrom, timeTo);
-    }
-</script>
+            const res = await fetch("http://localhost:8080/markers?" + params.toString());
+            markers = await res.json();
+        }
 
-<FilterBar {categories} on:filter={handleFilter} />
+        function handleFilter(event: CustomEvent<{
+            date: string | null;
+            category: string | null;
+            timeFrom: number | null;
+            timeTo: number | null;
+        }>) {
+            const { date, category, timeFrom, timeTo } = event.detail;
+            fetchMarkers(
+                date ?? "",
+                category ?? "",
+                timeFrom?.toString() ?? "",
+                timeTo?.toString() ?? ""
+            );
+        }
+        const attribution = '&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    </script>
 
-<div style="width:600px;height:500px;">
-    <Map options={{ center: [50.9375, 6.9603], zoom: 13 }}>
-        <TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
+    <FilterBar {categories} on:filter={handleFilter} />
 
-        {#each markers as marker}
-            <Marker latLng={[marker.lat, marker.lng]}>
-                <Popup>
-                    <h3>{marker.name}</h3>
-                    <p>{marker.address}</p>
-                    <p>{marker.category}</p>
-                    <p>{new Date(marker.dateTime).toLocaleString("de-DE")}</p>
-                </Popup>
-            </Marker>
-        {/each}
-    </Map>
-</div>
-<VolunteerList {markers}></VolunteerList>
+    <div style="width:100%;height:500px;">
+        <Map options={{ center: [50.9375, 6.9603], zoom: 13 }}>
+            <TileLayer
+                url={'https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png'}
+                options={{ attribution }}
+            />
+
+            {#each markers as marker}
+                <CircleMarker  latLng={[marker.lat, marker.lng]}>
+                    <Popup>
+                        <h3>{marker.name}</h3>
+                        <p>{marker.address}</p>
+                        <p>{marker.category}</p>
+                        <p>{new Date(marker.dateTime).toLocaleString("de-DE")}</p>
+                    </Popup>
+                </CircleMarker >
+            {/each}
+        </Map>
+    </div>
+    <VolunteerList {markers}></VolunteerList>
