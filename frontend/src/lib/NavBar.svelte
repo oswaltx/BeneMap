88ee57aee69dbd60ddf3bf50e3072a0b35ae809e
david_windows <xd@oswalt.de>
2026-04-09 16:44:30 +0200

    Create NavBar.svelte component for site navigation.

---
+++ frontend/src/lib/NavBar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Link from "./Link.svelte";
+</script>
+
+<nav>
+    <Link href="/">Home</Link>
+    <Link href="/map">Map</Link>
+    <Link href="/about">About</Link>
+</nav>
